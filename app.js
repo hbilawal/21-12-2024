617097f822efdcfed57776b842d8ee0872fd9bca
hingorobilawal <hingorobilawal786@gmail.com>
2024-12-23 09:41:05 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+
+// chapter no9-11
+
+
+// Q no1 Ans
+
+// if(city == "karachi"){
+//     console.log("welcome to the city of lights")
+// }
+
+// Q no2 Ans
+
+
+// let a = prompt("enter your gender");
+// if (gender == "male"){;
+//     alert("good morning sir");
+// }
+// let a = prompt("enter your gender");
+// if (gender == "female"){;
+//     alert("good morning ");
+// }
+
+
+// Q no4 Ans
+
+// if(fuel <=== "0.5litters")
+// console.log("please refill the fuel in your car")
+
+
+// Q no7 Ans
+// var secretno= "secretnumber 1to10"prompt("guess")
+// alert("Bingo!Correct")
+
+
